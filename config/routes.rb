d3240e9d9e10d078ecedc5337b0ca61e9ead07a4
oharada <oharada@iplab.cs.tsukuba.ac.jp>
2016-11-05 18:07:04 +0900

    Add feature:image uploader

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :items
+  resources :items do
+    member do
+      get 'image'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
